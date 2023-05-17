00adf2fef5fad9292448e2161222852c7f5b81ea
norlynj <nacastillo2@up.edu.ph>
2023-05-17 12:06:54 +0800

    comment factory bot

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,7 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
-  config.include FactoryBot::Syntax::Methods
+  # config.include FactoryBot::Syntax::Methods
 
   config.before(:suite) do
     ActiveJob::Base.queue_adapter = :test
