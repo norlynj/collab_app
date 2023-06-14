18e5b6a59f024d1a06b76456dc1c789da3d2ff00
norlynj <nacastillo2@up.edu.ph>
2023-06-14 21:27:16 +0800

    add comments for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   include BCrypt
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :tasks
   has_many :comments
   has_many :update_logs
@@ -19,50 +21,58 @@ class User < ApplicationRecord
 
   before_save :encrypt_password
 
-
   enum role: { group_leader: 'group_leader', task_leader: 'task_leader', member: 'member' }
 
+  # Encrypts the user's password before saving to the database
   def encrypt_password
     self.password_digest = Password.create(password)
   end
 
+  # Authenticates the user's password
   def authenticate(password)
     Password.new(password_digest) == password
   end
 
+  # Creates a new user with the given name and email, and generates a random password
+  # Returns the generated password
   def create_user_with_credentials(name, email)
     password = generate_password
     create(name: name, email: email, password: password)
     password
   end
 
+  # Generates a random password
   def generate_password
     SecureRandom.alphanumeric(8)
   end
 
+  # Updates the user's information with the given name, email, role, and label
   def update_user_information(name, email, role, label)
     update(name: name, email: email, role: role, label: label)
   end
 
+  # Assigns a task to the user
   def assign_task(task)
     assigned_tasks << task
   end
 
+  # Adds a comment to the specified task with the given note
   def add_comment(task, note)
     comments.create(task: task, note: note)
   end
 
+  # Retrieves assigned tasks based on a specific label
   def assigned_tasks_by_label(label)
     assigned_tasks.joins(:label).where(labels: { tag: label })
   end
 
+  # Retrieves assigned tasks based on a specific status
   def assigned_tasks_by_status(status)
     assigned_tasks.where(status: status)
   end
 
+  # Adds an update log entry for the specified task with the given note
   def add_update_log(task, note)
     update_logs.create(task: task, note: note)
   end
-
 end
-
