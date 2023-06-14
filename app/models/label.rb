990f36af4daf12240ea83d4db9b341766d1c78f0
norlynj <nacastillo2@up.edu.ph>
2023-06-14 22:14:18 +0800

    add comments for label

--- app/models/label.rb
+++ app/models/label.rb
@@ -3,10 +3,12 @@ class Label < ApplicationRecord
   # Validations
   validates :tag, presence: true, uniqueness: true
 
+  # Find tasks for a certain label
   def self.find_tasks_by_label(label)
     find_by(tag: label).tasks
   end
 
+  # Find tasks using a status
   def self.find_tasks_by_status(status)
     joins(:tasks).where(tasks: { status: status })
   end
