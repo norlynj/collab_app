20b7d1a67c7960408c3fcd4c1356458f29cd64e6
norlynj <nacastillo2@up.edu.ph>
2023-06-14 21:11:07 +0800

    Update tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :authenticate_user
+  before_action :authenticate_user!
   def create
     @task = Task.new(task_params)
     if @task.save
